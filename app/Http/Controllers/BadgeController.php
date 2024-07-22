752f0db1a5ff2677eb1ca9fe03ac502af2cf667b
Thiritin <me@thiritin.com>
2024-07-22 20:45:39 +0200

    fix policy

--- app/Http/Controllers/BadgeController.php
+++ app/Http/Controllers/BadgeController.php
@@ -120,6 +120,7 @@ class BadgeController extends Controller
 
     public function edit(Badge $badge, Request $request)
     {
+        Gate::authorize('update', $badge);
         return Inertia::render('Badges/BadgesEdit', [
             'canEdit' => $request->user()->can('update', $badge),
             'canDelete' => $request->user()->can('delete', $badge),
