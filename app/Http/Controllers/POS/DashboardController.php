de9cfcaa21b2ed9071ac6b63f16826b3a8e1d78b
Alex Ottr <alex@otter.foo>
2024-08-10 21:11:10 +0200

    add Dashboard scaffold

---
+++ app/Http/Controllers/POS/DashboardController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    public function __invoke()
+    {
+        return Inertia::render('Dashboard', [
+            
+        ]);
+    }
+}
---
+++ resources/js/Pages/POS/Dashboard.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+    <Head>
+        <title>POS - Dashboard</title>
+    </Head>
+    <div>
+        <h1>Dashboard</h1>
+    </div>
+</template>
