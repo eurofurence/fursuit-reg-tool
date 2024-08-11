9eba8e013d788ae4defebde6df786be2e712d430
Alex Ottr <alex@otter.foo>
2024-08-11 03:25:58 +0200

    add layout changes

--- resources/js/Layouts/POSLayout.vue
+++ resources/js/Layouts/POSLayout.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
 import Button from "primevue/button";
 import Menu from "primevue/menu";
+import { Link } from "@inertiajs/vue3";
 import DigitalClock from "@/Components/POS/DigitalClock.vue";
 import { ref } from "vue";
 
 const userMenu = ref();
 const userMenuItems = ref([
-    { label: 'Logout', icon: 'pi pi-sign-out' },
-    { label: 'Switch User', icon: 'pi pi-user' }
+    { label: 'Logout', icon: 'pi pi-sign-out', route: route('auth.logout'), method: 'POST' },
+    { label: 'Switch User', icon: 'pi pi-user', route: '', method: 'POST' },
 ]);
 
 const toggleUserMenu = (event) => {
     userMenu.value.toggle(event);
 };
-
 </script>
 
 <template>
@@ -24,7 +24,16 @@ const toggleUserMenu = (event) => {
                 <DigitalClock />
             </div>
             <Button type="button" icon="pi pi-user" @click="toggleUserMenu" aria-haspopup="true" aria-controls="overlay_menu" label="User X" />
-            <Menu ref="userMenu" id="overlay_menu" :model="userMenuItems" :popup="true" />
+            <Menu ref="userMenu" id="overlay_menu" :model="userMenuItems" :popup="true">
+                <template #item="{ item, props }">
+                    <Link :href="item.route" :method="item.method" v-ripple>
+                        <div class="flex items-center px-4 py-2">
+                            <span :class="item.icon"></span>
+                            <span class="ml-2">{{ item.label }}</span>
+                        </div>
+                    </Link>
+                </template>
+            </Menu>
         </div>
         <slot></slot>
     </div>
