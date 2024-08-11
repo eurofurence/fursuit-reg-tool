5d76ceeae7131444bc8cf224bbc208fdf4d76ce6
Alex Ottr <alex@otter.foo>
2024-08-11 03:24:07 +0200

    add dashboard button

--- resources/js/Pages/POS/Dashboard.vue
+++ resources/js/Pages/POS/Dashboard.vue
@@ -1,6 +1,7 @@
-<script setup lang="ts">
-import { Head } from "@inertiajs/vue3";
+<script lang="ts" setup>
+import { Head, Link } from "@inertiajs/vue3";
 import Layout from "@/Layouts/POSLayout.vue";
+import DashboardButton from "@/Components/POS/DashboardButton.vue";
 
 defineOptions({
     layout: Layout
@@ -11,7 +12,11 @@ defineOptions({
     <Head>
         <title>POS - Dashboard</title>
     </Head>
-    <div>
-        <h1>Dashboard</h1>
+    <div class="grow flex flex-col">
+        <div class="grow flex flex-wrap gap-10 justify-center items-center">
+            <DashboardButton label="Lookup Attendee" />
+            <DashboardButton label="Badge List" />
+            <DashboardButton label="Cash Register" />
+        </div>
     </div>
 </template>
