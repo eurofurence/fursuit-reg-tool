d8c2bd5aec586119446de31fb2b8bc76905d041f
Thiritin <me@thiritin.com>
2024-08-11 04:20:19 +0200

    fix auth I hate my life, this works now. FUUUU

--- app/Models/Machine.php
+++ app/Models/Machine.php
@@ -2,15 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\Access\Authorizable;
 
 /**
  * Machine describes a pos system
  */
 
-class Machine extends Authenticatable
+class Machine extends Model implements \Illuminate\Contracts\Auth\Authenticatable
 {
+    use Authenticatable, Authorizable;
     public $timestamps = false;
     protected $guarded = [];
 }
