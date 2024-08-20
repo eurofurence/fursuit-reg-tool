244dbac1704b5d0fe5222136a86e258355a094ff
A.Ottr <alex@otter.foo>
2024-08-21 00:26:45 +0200

    add back lookup logic

--- app/Http/Controllers/POS/AttendeeController.php
+++ app/Http/Controllers/POS/AttendeeController.php
@@ -19,9 +19,17 @@ class AttendeeController extends Controller
 
     public function lookupSubmit(Request $request): RedirectResponse
     {
-        print($request->get('attendeeId'));
-        $user = User::where('attendee_id', $request->get('attendeeId'))->first();
+        $user = User::where('attendee_id', $request->get('attendeeId'))->first()->exists();
         if (!$user) return redirect()->back()->withErrors(['attendeeId' => 'Could not find attendee']);
-        else return redirect()->back()->withErrors(['attendeeId' => 'Found User']);
+
+        else return redirect()->route('pos.attendee.show', ['attendeeId' => $request->get('attendeeId')]);
+    }
+
+    public function attendeeShow(string $attendeeId, Request $request):  Response
+    {
+        $user = User::where('attendee_id', $attendeeId)->first();
+        print_r($user);
+
+        return Inertia::render('POS/Attendee/Show', ['attendee' => $user]);
     }
 }
--- routes/pos.php
+++ routes/pos.php
@@ -10,5 +10,6 @@ Route::get('/', \App\Http\Controllers\POS\DashboardController::class)->name('das
 Route::prefix('/attendee')->name('attendee.')->group(function () {
     Route::get('/lookup', [\App\Http\Controllers\POS\AttendeeController::class, 'lookupForm'])->name('lookup');
     Route::post('/lookup', [\App\Http\Controllers\POS\AttendeeController::class, 'lookupSubmit'])->name('lookup.submit');
+    Route::get('/show/{attendeeId}', [\App\Http\Controllers\POS\AttendeeController::class, 'attendeeShow'])->name('show');
 
 });
