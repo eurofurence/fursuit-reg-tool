92779d7f02c8536f40ef34cde5d74745e4a1736e
Luna D. <luna@nighty.cloud>
2024-09-10 18:35:24 +0200

    basic route + empty view

--- resources/js/Pages/POS/Attendee/Show.vue
+++ resources/js/Pages/POS/Attendee/Show.vue
@@ -79,7 +79,7 @@ function handoutBadges() {
                 <h1 class="text-2xl font-bold">{{ attendee.name }} # {{ attendee.attendee_id }}</h1>
             </div>
             <div class="grid grid-cols-3 gap-4">
-                <DashboardButton label="Pay" :subtitle="formatEuroFromCents(attendee.wallet.balance *-1) +' Unpaid'" icon="pi pi-money-bill" route="#"></DashboardButton>
+                <DashboardButton label="Pay" :subtitle="formatEuroFromCents(attendee.wallet.balance *-1) +' Unpaid'" icon="pi pi-money-bill" route="/checkout"></DashboardButton>
                 <DashboardButton label="Handout" :subtitle="badgesReadyForHandout + ' to handout'" icon="pi pi-th-large" @click="showHandoutConfirmModal = true"></DashboardButton>
                 <DashboardButton label="Cancel" icon="pi pi-wallet" :route="route('pos.dashboard')"></DashboardButton>
             </div>
---
+++ resources/js/Pages/POS/Checkout/Show.vue
@@ -0,0 +1,20 @@
+<script setup>
+import POSLayout from "@/Layouts/POSLayout.vue";
+
+defineOptions({
+    layout: POSLayout,
+});
+
+const props = defineProps({
+    badges: Array,
+    fursuits: Array,
+    attendee: Object,
+});
+
+</script>
+
+<template>
+    <div>
+        <h1>uwu</h1>
+    </div>
+</template>
