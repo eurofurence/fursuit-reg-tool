d79b1ba62e45dca20f2f610bfdfba96f2a06f059
thiritin <me@thiritin.com>
2024-09-11 17:42:40 +0200

    ef fursuit badges in title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>Eurofurence Fursuit Badges</title>
 
         <!-- Scripts -->
         @routes
