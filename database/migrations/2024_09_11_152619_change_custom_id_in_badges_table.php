be4f2b549b6e192ad8b0127edb4462c6128128f9
thiritin <me@thiritin.com>
2024-09-11 19:08:26 +0200

    fix migration

--- database/migrations/2024_09_11_152619_change_custom_id_in_badges_table.php
+++ database/migrations/2024_09_11_152619_change_custom_id_in_badges_table.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
+        \App\Models\Badge\Badge::whereNotNull('custom_id')->update(['custom_id' => null]);
         Schema::table('badges', function (Blueprint $table) {
             $table->string('custom_id')->nullable()->unique()->change();
         });
