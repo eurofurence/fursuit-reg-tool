9f3e8008a5c468ee458e9ee78a4e2cae97ad6dcc
Thiritin <me@thiritin.com>
2025-09-03 09:33:13 +0200

    csrf exclude

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -60,6 +60,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
         $middleware->validateCsrfTokens(except: [
+            'pos/*',
             'pos/auth/machine/status/update',
             'pos/auth/machine/status',
             'pos/auth/printer**',
