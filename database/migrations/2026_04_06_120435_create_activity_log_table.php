5d26c2fa6637f02e13d339650fadafa6719d0af1
Tim <72736935+TLTimeplex@users.noreply.github.com>
2026-04-06 12:06:59 +0200

    Create migration for activity log table and rename existing table

---
+++ database/migrations/2026_04_06_120435_create_activity_log_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        // renaming old table to keep old logs, but prevent conflicts with new activity log table
+        if (Schema::hasTable('activity_log')) {
+            Schema::rename('activity_log', 'old_activity_log');
+        }
+
+        Schema::create('activity_log', function (Blueprint $table) {
+            $table->id();
+            $table->string('log_name')->nullable()->index();
+            $table->text('description');
+            $table->nullableMorphs('subject', 'subject');
+            $table->string('event')->nullable();
+            $table->nullableMorphs('causer', 'causer');
+            $table->json('attribute_changes')->nullable();
+            $table->json('properties')->nullable();
+            $table->timestamps();
+        });
+    }
+};
