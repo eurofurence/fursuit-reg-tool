27b4a8189322252daf3af0962783dc48d5b7c32a
Tim <72736935+TLTimeplex@users.noreply.github.com>
2026-04-06 12:13:31 +0200

    Refactor activity log migration to add attribute_changes column and remove old table renaming logic

--- database/migrations/2026_04_06_120435_create_activity_log_table.php
+++ database/migrations/2026_04_06_120435_create_activity_log_table.php
@@ -8,21 +8,8 @@ return new class extends Migration
 {
     public function up(): void
     {
-        // renaming old table to keep old logs, but prevent conflicts with new activity log table
-        if (Schema::hasTable('activity_log')) {
-            Schema::rename('activity_log', 'old_activity_log');
-        }
-
-        Schema::create('activity_log', function (Blueprint $table) {
-            $table->id();
-            $table->string('log_name')->nullable()->index();
-            $table->text('description');
-            $table->nullableMorphs('subject', 'subject');
-            $table->string('event')->nullable();
-            $table->nullableMorphs('causer', 'causer');
+        Schema::table('activity_log', function (Blueprint $table) {
             $table->json('attribute_changes')->nullable();
-            $table->json('properties')->nullable();
-            $table->timestamps();
         });
     }
 };
