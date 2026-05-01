53258c58e4bc1db244063540dd7f57e6dff4e96f
Tim <72736935+TLTimeplex@users.noreply.github.com>
2026-05-01 08:53:41 +0200

    Fix Testing III.2

--- database/migrations/2025_09_23_194213_change_user_to_event_user_in_user_achievements_table.php
+++ database/migrations/2025_09_23_194213_change_user_to_event_user_in_user_achievements_table.php
@@ -62,7 +62,7 @@ return new class extends Migration
             $table->unsignedBigInteger('event_user_id')->nullable(false)->index()->change();
             $table->unique(['fursuit_id', 'event_user_id']);
 
-            $table->dropIndex('idx_user_catches_user_created')
+            $table->dropIndex('idx_user_catches_user_created');
 
             $table->dropUnique('user_catches_user_id_fursuit_id_unique');
             $table->dropForeign(['user_id']);
