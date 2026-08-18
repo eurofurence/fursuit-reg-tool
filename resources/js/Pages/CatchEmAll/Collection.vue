6dcff4a801c587fc889001717061cac146bb4e33
Tim <72736935+TLTimeplex@users.noreply.github.com>
2026-08-18 10:52:07 +0200

    Adding a short shoutout for the icons

--- resources/js/Pages/CatchEmAll/Collection.vue
+++ resources/js/Pages/CatchEmAll/Collection.vue
@@ -212,6 +212,7 @@ function openProfile(suit: Suit) {
             </div>
             <p v-if="!bySpecies.length" class="cea-hint">Nothing at that ranking yet.</p>
         </template>
+        <p class="cea-hint" style="margin-top: 14px">Icons by Nighty &gt;:3</p>
     </CatchEmAllLayout>
 </template>
 
